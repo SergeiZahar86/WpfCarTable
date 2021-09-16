b47c2f33d7e399f7f24705c7fa3e06024069172a
Сергей Захаров <sergei86zahar@mail.ru>
2021-09-16 22:38:41 +0700

    Добавьте файлы проекта.

---
+++ Entities/ModelCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class ModelCar
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public BrandCar Brand { get; set; }
+    }
+}
---
+++ Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public double Proceeds { get; set; }
+        public DateTime Date { get; set; }
+        public ModelCar Model_Car { get; set; }
+    }
+}
