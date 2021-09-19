643c1c1c0a3e8f619cebe538f1cb51f1a47874c8
Сергей Захаров <sergei86zahar@mail.ru>
2021-09-19 21:09:15 +0700

    Подсветка цветом

---
+++ WpfCarTable/Service/NameToBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfCarTable.Service
+{
+    public class NameToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((decimal)value >= 25000000)
+            {
+                return Brushes.LightGreen;
+            }
+            else
+            {
+                return Brushes.Transparent;
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
